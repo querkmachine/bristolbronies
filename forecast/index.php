e8f9fd8386e2eea09c1cf78ce36413a42b16e0dc
Grey Hargreaves <grey@goramandvincent.com>
2013-07-08 11:56:20 +0100

    Forecast tweaks/fixes

--- forecast/index.php
+++ forecast/index.php
@@ -60,6 +60,7 @@ if($latitude && $longitude && $timestamp) {
 
 ?>
         <div class="forecast-icon-wrapper">
+          <span class="beta-tag"><a href="/news/beta-forecast/">beta</a></span>
           <?php if($basecloud) { ?><span class="basecloud"></span><?php } ?>
           <?php for($i=0;$i<count($icons);$i++) { ?><span class="forecast-<?php echo $icons[$i]; ?>"></span><?php } ?>
         </div>
