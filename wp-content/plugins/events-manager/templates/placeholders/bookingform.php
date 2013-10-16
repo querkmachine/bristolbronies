47e0c86e3a0e3be60bd17e8aadbff0c23e74eaa6
Grey Hargreaves <grey@goramandvincent.com>
2013-10-16 22:02:51 +0100

    Modified design and behaviour of booking form a little

--- wp-content/plugins/events-manager/templates/placeholders/bookingform.php
+++ wp-content/plugins/events-manager/templates/placeholders/bookingform.php
@@ -45,7 +45,7 @@ if( !$is_open && !is_user_logged_in() && $EM_Event->get_bookings()->is_open(true
 		<?php echo $EM_Notices; ?>
 		<?php if( $tickets_count > 0) : ?>
 			<?php //Tickets exist, so we show a booking form. ?>
-			<form class="em-booking-form" name='booking-form' method='post' action='<?php echo apply_filters('em_booking_form_action_url',''); ?>#em-booking'>
+			<form class="em-booking-form" name='booking-form' method='post' action='<?php echo apply_filters('em_booking_form_action_url',''); ?>'>
 			 	<input type='hidden' name='action' value='booking_add'/>
 			 	<input type='hidden' name='event_id' value='<?php echo $EM_Event->event_id; ?>'/>
 			 	<input type='hidden' name='_wpnonce' value='<?php echo wp_create_nonce('booking_add'); ?>'/>
