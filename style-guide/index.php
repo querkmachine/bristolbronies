126f5fc1f3277527d130d57ed8f0262e2f221291
Grey Hargreaves <shaun.hargreaves@gmail.com>
2014-03-23 10:53:30 +0000

    Moved Ashman styles into theme directory

--- style-guide/index.php
+++ style-guide/index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
 <title>Pattern Primer</title>
-<link rel="stylesheet" href="/assets/css/stylesheet.css">
+<link rel="stylesheet" href="/wp-content/themes/ashman/assets/css/stylesheet.css">
 <style>
 body {
     padding: 2%;
@@ -62,9 +62,9 @@ endforeach;
 ?>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="/assets/js/jquery-1.11.0.min.js"><\/script>')</script>
-  <script src="/assets/js/jquery.fitvids.js"></script>
-  <script src="/assets/js/script.js"></script>
+  <script>window.jQuery || document.write('<script src="/wp-content/themes/ashman/assets/js/jquery-1.11.0.min.js"><\/script>')</script>
+  <script src="/wp-content/themes/ashman/assets/js/jquery.fitvids.js"></script>
+  <script src="/wp-content/themes/ashman/assets/js/script.js"></script>
 
 </body>
 </html>
