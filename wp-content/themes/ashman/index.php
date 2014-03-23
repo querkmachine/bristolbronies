2157c7eadd99c31312eda88e682f685d6ac38b86
Grey Hargreaves <shaun.hargreaves@gmail.com>
2014-03-23 19:53:51 +0000

    Started makin pretty pages up on those bitch-ass hos

--- wp-content/themes/ashman/index.php
+++ wp-content/themes/ashman/index.php
@@ -7,8 +7,19 @@
   if(have_posts()) :
     while(have_posts()) : the_post(); 
 ?>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+        <div class="row">
+          <article class="post post--wide">
+            <header class="post__header">
+              <h1 class="post__title"><?php the_title(); ?></h1>
+            </header>
+            <div class="post__body">
+              <?php the_content(); ?>
+            </div>
+          </article>
+          <div class="sidebar">
+            yes
+          </div>
+        </div>
 <?php
     endwhile;
   endif; 
