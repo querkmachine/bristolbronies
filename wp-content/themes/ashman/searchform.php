1860f2f376d516192b33f4063966f7caf02b4a31
Grey Hargreaves <shaun.hargreaves@gmail.com>
2014-03-24 22:44:22 +0000

    Add autofocus to search field

--- wp-content/themes/ashman/searchform.php
+++ wp-content/themes/ashman/searchform.php
@@ -2,7 +2,7 @@
   <form role="search" id="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
     <label>
       <span class="hidden">Search for:</span>
-      <input type="search" class="search-form__field" placeholder="Search" value="<?php echo get_search_query() ?>" name="s">
+      <input type="search" class="search-form__field" placeholder="Search" value="<?php echo get_search_query() ?>" name="s" autofocus>
     </label>
     <button type="submit" class="search-form__submit button--positive hidden"><i class="fa fa-search"></i> Search</button>
   </form>
