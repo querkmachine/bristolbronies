13d2b98cd5ccf641b4e2be52a549bbb3146fa9b1
Grey Hargreaves <shaun.hargreaves@gmail.com>
2014-03-30 16:59:37 +0100

    Add community stylings

--- wp-content/themes/ashman/archive-community.php
+++ wp-content/themes/ashman/archive-community.php
@@ -33,9 +33,17 @@
         <?php 
             break;
             case 'video':
+              $thumbnail = wp_get_attachment_image_src(get_field("community_thumb"), "thumbnail");
+              $hires = wp_get_attachment_image_src(get_field("community_thumb"), "large");
         ?>
           <figure class="media-grid__item media-grid__item--video">
-            
+            <a href="<?php the_permalink(); ?>">
+              <img class="lazy" src="<?php echo $thumbnail[0]; ?>" data-src="<?php echo $hires[0]; ?>" alt="<?php the_title(); ?>">
+              <div class="media-grid__meta">
+                <i class="fa fa-play-circle-o fa-fw fa-4x"></i>
+                <h4><?php the_title(); ?></h4>
+              </div>
+            </a>
           </figure>
         <?php 
             break;
