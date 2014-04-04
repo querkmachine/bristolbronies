5925ab83ec18ba3efcff4b2d685c7eb47a3dd360
Grey Hargreaves <shaun.hargreaves@gmail.com>
2014-04-04 14:32:03 +0100

    Update admin panel plugin to use bb_ prefix

--- wp-content/plugins/bb-admin-panel/bb-admin-panel.php
+++ wp-content/plugins/bb-admin-panel/bb-admin-panel.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Bristol Bronies Ashman WordPress Administration Panel
+Plugin Name: Bristol Bronies WordPress Administration Panel
 Plugin URI: http://bristolbronies.co.uk/
 Description: WP admin panel tweaks for the Bristol Bronies website.
 Author: Grey Hargreaves
@@ -9,25 +9,25 @@ Version: 1.0
 */
 
 /* Custom admin panel CSS */
-function ashman_admin_theme_style() {
+function bb_admin_theme_style() {
   wp_enqueue_style('my-admin-theme', plugins_url('wp-admin.css', __FILE__));
 }
 
-add_action('admin_enqueue_scripts', 'ashman_admin_theme_style');
-add_action('login_enqueue_scripts', 'ashman_admin_theme_style');
+add_action('admin_enqueue_scripts', 'bb_admin_theme_style');
+add_action('login_enqueue_scripts', 'bb_admin_theme_style');
 
 /* Add meet dashboard widget */
-function ashman_meet_dashboard_widget() {
+function bb_meet_dashboard_widget() {
   wp_add_dashboard_widget(
-    'ashman_upcoming_meets',
+    'bb_upcoming_meets',
     'Upcoming Bristol Bronies Meets',
-    'ashman_meet_dashboard_widget_content'
+    'bb_meet_dashboard_widget_content'
   );
 }
 
-add_action('wp_dashboard_setup', 'ashman_meet_dashboard_widget');
+add_action('wp_dashboard_setup', 'bb_meet_dashboard_widget');
 
-function ashman_meet_dashboard_widget_content() {
+function bb_meet_dashboard_widget_content() {
   $posts=query_posts('post_type=meet&meta_key=meet_start_time&orderby=meta_value_num&order=ASC&posts_per_page=-1');
   if(have_posts()) :
     $output = '<ul>';
@@ -37,7 +37,7 @@ function ashman_meet_dashboard_widget_content() {
         $output .= '<h4><a href="/wp-admin/post.php?post=' . get_the_ID() . '&action=edit">' . get_the_title() . '</a>';
         if(get_post_status() != "publish") { $output .= ' - ' . get_post_status(); }
         $output .= '</h4>';
-        $output .= '<small>' . ashman_meet_dates(get_field("meet_start_time", get_the_ID()), get_field("meet_end_time", get_the_ID())) . '</small>';
+        $output .= '<small>' . bb_meet_dates(get_field("meet_start_time", get_the_ID()), get_field("meet_end_time", get_the_ID())) . '</small>';
         $output .= '</li>';
       }
     endwhile;
@@ -47,8 +47,8 @@ function ashman_meet_dashboard_widget_content() {
 }
 
 /* Remove unused dashboard widgets */
-function ashman_remove_dashboard_widgets() {
+function bb_remove_dashboard_widgets() {
   remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
 }
 
-add_action('wp_dashboard_setup', 'ashman_remove_dashboard_widgets');
+add_action('wp_dashboard_setup', 'bb_remove_dashboard_widgets');
