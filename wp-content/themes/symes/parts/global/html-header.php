6fefdf19002493033ee7c1b8d2e0c89c907acde4
Grey Hargreaves <shaun.hargreaves@gmail.com>
2014-05-18 21:01:03 +0100

    Add hCalendar profile to page header

--- wp-content/themes/symes/parts/global/html-header.php
+++ wp-content/themes/symes/parts/global/html-header.php
@@ -11,6 +11,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="robots" content="all, noydir, noodp">
   <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="profile" href="http://microformats.org/profile/hcalendar">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
   <!-- Content metadata -->
