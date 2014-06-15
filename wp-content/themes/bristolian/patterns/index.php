2c9b84dabbaa4896f5e86a8f5bc32bb5f0775464
Grey Hargreaves <shaun.hargreaves@gmail.com>
2014-06-15 18:08:51 +0100

    Add new pattern primer

---
+++ wp-content/themes/bristolian/patterns/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<title>Pattern Primer</title>
+<link rel="stylesheet" href="/wp-content/themes/ashman/assets/css/stylesheet.css">
+<style>
+body {
+    padding: 2%;
+    background-color: #fff;
+}
+.pattern {
+    margin-bottom: 2em;
+    border: 1px solid #dedede;
+    clear: both;
+    overflow: hidden;
+}
+.pattern .display {
+    padding: 2%;
+}
+.pattern .source {
+    padding: 0.5em 1em;
+    background: #dedede;
+}
+.pattern .source a {
+    float: right;
+}
+.pattern .source textarea {
+    margin-top: 0.5em;
+    width: 100%;
+    resize: vertical;
+    font-family: monospace;
+    font-size: smaller;
+}
+</style>
+</head>
+<body>
+
+<?php
+$files = array();
+$handle=opendir('patterns');
+while (false !== ($file = readdir($handle))):
+    if(stristr($file,'.html')):
+        $files[] = $file;
+    endif;
+endwhile;
+sort($files);
+foreach ($files as $file):
+    echo '<div class="pattern" id="'.str_replace(".html", "", $file).'"">';
+    echo '<details class="source">';
+    echo '<summary>'.$file.' <a href="#'.str_replace(".html", "", $file).'">#</a></summary>';
+    echo '<textarea rows="6" cols="30">';
+    echo htmlspecialchars(file_get_contents('patterns/'.$file));
+    echo '</textarea>';
+    echo '</details>';
+    echo '<div class="display">';
+    include('patterns/'.$file);
+    echo '</div>';
+    echo '</div>';
+endforeach;
+?>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="/wp-content/themes/ashman/assets/js/jquery-1.11.0.min.js"><\/script>')</script>
+  <script src="/wp-content/themes/ashman/assets/js/jquery.fitvids.js"></script>
+  <script src="/wp-content/themes/ashman/assets/js/script.js"></script>
+
+</body>
+</html>
